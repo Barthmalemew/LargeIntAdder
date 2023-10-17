6d87522536d3f89603bc976e97948e5ffccbffc8
Barthmalemew <78800375+Barthmalemew@users.noreply.github.com>
2023-10-17 17:17:26 -0400

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -17,33 +17,70 @@ void getNum(int array[]);
 // Prints a large integer to the screen. Leading zeros are not printed.
 void printNum(int array[]);
 
+// Contains the math that adds 2 arrays.
+void solveNum(int array1[], int array2[], int arraysum[]);
+
 
 int main()
 {
 	// TODO: Write code for the main function
+	int num1[MAXSIZE];
+	int num2[MAXSIZE];
+	int sum[MAXSIZE];
+
+	
+
+	initArray(num1); //triggers the init array function, changing the array values.
+
+	initArray(num2);
+
+	getNum(num1);
+
+	getNum(num2);
+
+	solveNum(num1, num2, sum);
 
 	cout << endl;
+
 	return 0;
+
 }
 
 
 void initArray(int array[])
 {
-	// TODO: Write code for the function
+	
+	for (int i = 0; i < MAXSIZE; i++)
+		array[i] = 0; 
 }
 
 
 void printNum(int array[])
 {
-	// TODO: Write code for the function
+	bool leadZero = true;
+
+	printf("The large sum is: ");
+
+	for (int i = 0; i < MAXSIZE; i++)
+	{
+		if (array[i] != 0)
+		{
+			leadZero = false;
+		}
+		if(leadZero == false)
+		{
+			printf("%d", array[i]);
+		}
+	}
 }
 
 
 void getNum(int array[])
 {
-	char input[MAXSIZE + 1];
+	char input[MAXSIZE + 1]; // + 1 for '\0'
 	int length;
 	bool inputOkay;
+	
 
 	do
 	{
@@ -75,8 +112,59 @@ void getNum(int array[])
 		}
 
 	} while (!inputOkay);
+	
+	//ari solved the getnum!!!! remember for credit
+	int end = 0;
+	int temp[MAXSIZE];
+
+	for (int i = 0; input[i] != '\0'; i++)
+	{
+		temp[i] = input[i] - '0';
+		end = i + 1;
+	}
+
+	int start = MAXSIZE - (end);
+
+	for (int i = 0; start < MAXSIZE; start++)
+	{
+		array[start] = temp[i];
+		i++;
+	}
+}
 
 
-	// TODO: Write code for the remaining portion of the function
+void solveNum(int array1[],int array2[],int arraysum[])
+{
+	bool counter = false;
+	bool overflow = false;
+	
+	for (int i = MAXSIZE - 1; i >= 0; i--) 
+	{
+		if (counter == true)
+		{
+			arraysum[i] = array1[i] + array2[i] + 1;
+			counter = false;
+		}
+		else
+		{
+			arraysum[i] = array1[i] + array2[i];
+		}
 
+		if (arraysum[i] > 9 && i == 0)
+		{
+			printf("Overflow error");
+			overflow = true;
+		}
+
+		if (arraysum[i] > 9)
+		{
+			arraysum[i] = arraysum[i] % 10;
+			counter = true;
+		}
+	}
+
+	if (overflow == false)
+	{
+		printNum(arraysum);
+	}
 }
